eb1c93bc1c1be8e2cf84762999c62f0fa04e71a8
Denis Biryukov <biryukov.denis88@gmail.com>
2020-12-10 13:21:08 +0300

    added 2 button and second display for the change background colors and button to return

--- Torch and traffic light/ViewController.swift
+++ Torch and traffic light/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = 0
+    
+//    view.backgroundColor = .grey
+    
+    fileprivate func updareUI() {
+        switch lightOn {
+        case 0:
+            view.backgroundColor = .white
+        case 1:
+            view.backgroundColor = .yellow
+        case 2:
+            view.backgroundColor = .green
+        case 3:
+            view.backgroundColor = .cyan
+        default:
+            view.backgroundColor = .black
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updareUI()
     }
 
-
+    @IBAction func buttonPresed(_ sender: UIButton) {
+        lightOn = lightOn < 3 ? lightOn + 1 : -1
+        updareUI()
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+    }
 }
 
